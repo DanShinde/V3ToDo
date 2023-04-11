556b10e2d6a25df627771d40070b4d3323cb3883
DanShinde <pravin113347@gmail.com>
2023-04-11 22:05:33 +0530

    cors allowed

--- acore/acore/settings.py
+++ acore/acore/settings.py
@@ -58,8 +58,29 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://webpack.pythonanywhere.com",
-    "http://127.0.0.1:8000",
+    "http://localhost:8000",
     "http://localhost",
+    "http://localhost:3000",
+]
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
 ]
 
 ROOT_URLCONF = 'acore.urls'
